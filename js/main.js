25344b3d92c0a1c075551e50dd5976dd94e1232e
opifex <opifexM@outlook.com>
2023-07-02 15:01:19 -0400

    7.15 - picture.js added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import createPicture from './data.js';
+import {drawPicture} from './picture.js';
 
 const pictureArray = Array.from({length: 25}, createPicture);
-console.log(JSON.stringify(pictureArray, null, 2));
+// console.log(JSON.stringify(pictureArray, null, 2));
+drawPicture(pictureArray);
+
+
