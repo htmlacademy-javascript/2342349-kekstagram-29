3dcd84436ebe844f63c0e9f0fad06294d6aeea49
opifex <opifexM@outlook.com>
2023-07-14 12:53:15 -0400

    9.15 - formValidator.js added

---
+++ js/imageFormValidator/domElements.js
@@ -0,0 +1,14 @@
+export const body = document.querySelector('body');
+export const imageUploadInput = document.querySelector('.img-upload__input');
+export const imageUploadCancel = document.querySelector('.img-upload__cancel');
+export const imageUploadOverlay = document.querySelector('.img-upload__overlay');
+export const imageUploadForm = document.querySelector('.img-upload__form');
+export const imageUploadFormTag = document.querySelector('.text__hashtags');
+export const imageUploadFormText = document.querySelector('.text__description');
+export const imageUploadPreview = document.querySelector('.img-upload__preview img');
+export const imageUploadScaleControlValue = document.querySelector('.scale__control--value');
+export const imageUploadScaleControlSmaller = document.querySelector('.scale__control--smaller');
+export const imageUploadScaleControlBigger = document.querySelector('.scale__control--bigger');
+export const effectLevelSliderElement = document.querySelector('.effect-level__slider');
+export const effectLevelRadioButtons = document.querySelectorAll('.effects__radio');
+export const effectLevelValueElement = document.querySelector('.effect-level__value');
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import createPicture from './data.js';
 import {drawPicture} from './picture.js';
-import {initialize as initializeFormValidator} from './formValidator.js';
+import {initializeImageFormValidator} from './imageFormValidator/index.js';
 
 const pictureArray = Array.from({length: 25}, createPicture);
 drawPicture(pictureArray);
 
-initializeFormValidator();
+initializeImageFormValidator();
